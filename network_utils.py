convert yhad to vector of 0 and 1s in evaluate_parameters fucntion, fix precision function

network_utils.py
@@ -26,9 +26,9 @@ def generate_W_matrix(G, y):
         res = np.dot(np.dot(np.linalg.pinv(np.dot(G.T, G)), G.T), y)
         return res
     except np.linalg.LinAlgError:
-        print("SVD convergence error.")
+        print("SVD Convergence Error.")
         _, m = G.shape
-        # TODO fill with random numbers?
+        # TODO is it OK to fill with random numbers?
         res = np.random.uniform(size=m)
         return res
 
@@ -46,6 +46,7 @@ def evaluate_parameters(V, gama, X, y):
     G = generate_G_matrix(X, V, gama)
     W = generate_W_matrix(G, y)
     yhad = generate_yhad_matrix(G, W)
+    yhad = np.where(yhad > 0, 1, -1)
     error = mse(y, yhad)
     return error
 
@@ -54,6 +55,6 @@ def get_precision(X, y, V, gama):
     G = generate_G_matrix(X, V, gama)
     W = generate_W_matrix(G, y)
     yhad = generate_yhad_matrix(G, W)
-    yhad = np.where(yhad >= 0, 1, -1)
-    temp = np.abs(np.subtract(y, yhad))
-    return np.sum(temp) / 2
+    yhad = np.where(yhad > 0, 1, -1)
+    L, _ = G.shape
+    return mse(y, yhad) / (2 * L)