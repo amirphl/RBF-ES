e6683aa34f4df4cf6ec47d286168f76580e0a904
amirphl <amirphl@aut.ac.ir>
2019-06-11 22:48:25 +0430

    plot regression and classification results

---
+++ plotter.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_two_class_classification(X, y, V, yhad, scales):
+    L, n = X.shape
+    assert n == 2
+
+    for i in range(n):
+        X[:, i] = X[:, i] * scales[i]
+
+    mapping = {-1: ("red", "."), 1: ("blue", "."), -2: ("orange", "."), 2: ("black", ".")}
+
+    for i in range(L):
+        if yhad[i] != y[i]:
+            if yhad[i] == 1:
+                yhad[i] = -2
+            else:
+                yhad[i] = 2
+
+    for c in np.unique(yhad):
+        d = X[yhad == c]
+        plt.scatter(d[:, 0], d[:, 1], c=mapping[c][0])
+    plt.scatter(V[:, 0], V[:, 1], c="green")
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.suptitle('2 class classification')
+    plt.show()
+    plt.savefig('2 class classification output.png')
+
+
+def plot_regression_data(yhad):
+    L = yhad.shape
+    plt.scatter(range(L[0]), yhad)
+    plt.xlabel('position of data')
+    plt.ylabel('regression bound')
+    plt.suptitle('regression output')
+    plt.savefig('regression output.png')
+    plt.show()
